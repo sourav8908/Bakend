8736ccd66851cbeb557c759ecc5197b63d66f36d
Sourav <mohantysourav537@gmail.com>
2024-04-13 13:03:13 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandeler = (reqesthandler) => {
+    (req,res,next) => {
+        Promise.resolve(reqesthandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandeler}
+
+
+// const asyncHandeler = () => {}
+// const asyncHandeler = (func) => () => {}
+// const asyncHandeler = (func) => async () => {}
+
+// const asyncHandeler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+
+//           success: false,
+//           Message: err.Message
+
+//         })
+//     }
+// }
